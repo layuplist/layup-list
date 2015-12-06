da4242098b1b347d637b9ed7b59d160b6ff43288
cheniel <danielchen627@gmail.com>
2015-12-05 22:00:45 -0500

    add .layuplist.com to ALLOWED_HOSTS

--- layup_list/settings.py
+++ layup_list/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ["DEBUG"] == "True"
+DEBUG = os.environ['DEBUG'] == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.layuplist.com']
 
 # Application definition
 INSTALLED_APPS = [
