ba685193219ba056ecd918beee571755c87fdb07
cheniel <danielchen627@gmail.com>
2015-12-23 03:00:19 -0800

    added compute_initial_scores.py

--- scripts/dev/import_data.sh
+++ scripts/dev/import_data.sh
@@ -15,3 +15,6 @@ python scripts/importers/import_medians.py
 # import reviews
 python scripts/importers/import_reviews.py
 python scripts/importers/import_layups_as_reviews.py
+
+# compute the initial scorings
+python scripts/compute/compute_initial_scores.py
