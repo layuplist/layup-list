a2014b36d5bdf917ecc6a9eb400d87cbb5602a54
cheniel <danielchen627@gmail.com>
2016-02-08 15:23:59 -0500

    add value to vote

--- web/models/vote.py
+++ web/models/vote.py
@@ -12,6 +12,7 @@ class Vote(models.Model):
             (LAYUP, "Layup"),
         )
 
+    value = models.IntegerField(default=0)
     course = models.ForeignKey("Course", on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.CharField(max_length=8, choices=Categories.CHOICES, db_index=True)
