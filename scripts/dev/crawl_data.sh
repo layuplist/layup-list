925faf48053a74eb32de4e0d116e38ddc47625b8
cheniel <danielchen627@gmail.com>
2016-02-09 00:09:43 -0500

    update crawl_term in crawl_data.sh

--- scripts/dev/crawl_data.sh
+++ scripts/dev/crawl_data.sh
@@ -9,6 +9,6 @@ phantomjs $wd/scripts/crawlers/crawl_orc_courses.js;
 cd medians;
 phantomjs $wd/scripts/crawlers/crawl_medians.js 13x 13f 14w 14s 14x 14f 15w 15s 15x 15f;
 cd ../terms;
-phantomjs $wd/scripts/crawlers/crawl_term.js 2016 1;
+phantomjs $wd/scripts/crawlers/crawl_term.js 2016 S;
 cd $wd
 python scripts/crawlers/crawl_course_pages.py
