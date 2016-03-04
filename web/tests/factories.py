2d46223d6d5493581a86f8ee3ea8076c120e3118
cheniel <danielchen627@gmail.com>
2016-03-04 17:08:53 -0500

    fix bug with factories not writing to test database

--- web/tests/factories.py
+++ web/tests/factories.py
@@ -2,8 +2,11 @@ from django.contrib.auth.models import User
 import factory
 from web import models
 from lib import constants
+from django.db.models.signals import post_save
+
 
 class UserFactory(factory.django.DjangoModelFactory):
+
     class Meta:
         model = User
 
@@ -11,9 +14,23 @@ class UserFactory(factory.django.DjangoModelFactory):
     email = factory.Faker("email")
     first_name = factory.Faker("first_name")
     last_name = factory.Faker("last_name")
-    password = factory.Faker("password")
+    is_active = True
+
+    @classmethod
+    def _prepare(cls, create, **kwargs):
+        # thanks: https://gist.github.com/mbrochh/2433411
+        password = factory.Faker('password')
+        if 'password' in kwargs:
+            password = kwargs.pop('password')
+        user = super(UserFactory, cls)._prepare(create, **kwargs)
+        user.set_password(password)
+        if create:
+            user.save()
+        return user
+
 
 class CourseFactory(factory.django.DjangoModelFactory):
+
     class Meta:
         model = models.Course
 
@@ -23,28 +40,35 @@ class CourseFactory(factory.django.DjangoModelFactory):
     url = factory.Faker("url")
     description = factory.Faker("text")
 
+
 class CourseOfferingFactory(factory.django.DjangoModelFactory):
+
     class Meta:
         model = models.CourseOffering
+
+    course = factory.SubFactory(CourseFactory)
 
     term = constants.CURRENT_TERM
     course_registration_number = factory.Faker("random_number")
     section = factory.Faker("random_number")
     period = "2A"
-    course = CourseFactory()
+
 
 class ReviewFactory(factory.django.DjangoModelFactory):
+
     class Meta:
         model = models.Review
 
-    course = CourseFactory()
-    user = UserFactory()
+    course = factory.SubFactory(CourseFactory)
+    user = factory.SubFactory(UserFactory)
+
     professor = factory.Faker("name")
     term = constants.CURRENT_TERM
     comments = factory.Faker("paragraph")
 
 
 class DistributiveRequirementFactory(factory.django.DjangoModelFactory):
+
     class Meta:
         model = models.DistributiveRequirement
 
