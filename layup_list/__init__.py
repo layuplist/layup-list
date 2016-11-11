68555cc490adfb8bde6b4c729114502f7b8d6449
cheniel <danielchen627@gmail.com>
2016-11-10 20:17:06 -0800

    Fix import error

--- layup_list/__init__.py
+++ layup_list/__init__.py
@@ -1,5 +1,3 @@
-# from __future__ import absolute_import, unicode_literals
-
-from layup_list.tasks import app as celery_app
+from layup_list.celery import app as celery_app
 
 __all__ = ['celery_app']
