4a00d538ff07c38be298dad39c43429665bbfeca
Ziray Hao <ziray.hao@gmail.com>
2025-03-27 13:25:50 -0700

    Change sender email

--- layup_list/settings.py
+++ layup_list/settings.py
@@ -151,7 +151,7 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
-DEFAULT_FROM_EMAIL = 'support@layuplist.com'
+DEFAULT_FROM_EMAIL = 'contrarians@dartmouth.edu'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
